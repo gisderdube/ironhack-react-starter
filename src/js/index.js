2d560012ccf597f5bc1c852d39f53467a734f10e
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-10 12:04:40 +0200

    styles setup

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 const appContainer = document.getElementById('app')
-ReactDOM.render(<h1>Our first React Code </h1>, appContainer)
+ReactDOM.render(<h1>Our first React Codeadaedqweqwqqwe </h1>, appContainer)
