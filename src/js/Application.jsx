c28e36a086518b0700ca3b565874719bb0a6e755
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-10 12:30:11 +0200

    added className to Application

--- src/js/Application.jsx
+++ src/js/Application.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 class Application extends React.Component {
     render() {
         return (
-            <div>
+            <div className="application">
                 <h1>Our first React Component, which is in another file!</h1>
             </div>
         )
