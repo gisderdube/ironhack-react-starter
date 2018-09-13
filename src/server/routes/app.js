40ef671f0b34b84b1ac7d7df9e880f2fc338139c
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-13 17:23:29 +0200

    fixed production routing

--- src/server/routes/app.js
+++ src/server/routes/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const path = require('path')
 const router = express.Router()
 
+const config = require('../config')
+
 router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../dist/index.html'))
+    const indexPath = config.IS_PRODUCTION ? '../public/index.html' : '../../../dist/index.html'
+    res.sendFile(path.join(__dirname, indexPath))
 })
 
 module.exports = router
